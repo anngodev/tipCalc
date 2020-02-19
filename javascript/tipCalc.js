5457abef347e394d77a8e525db024c6519def895
Andy <ngoan_230@yahoo.com>
2020-02-19 16:23:13 -0500

    Added boolean for values and arithmetic to calculate total

--- javascript/tipCalc.js
+++ javascript/tipCalc.js
@@ -11,6 +11,30 @@ function calculateTip() {
     var serviceQuality = document.getElementById("serviceQuality").value;
     var numPeople = document.getElementById("totalPeople").value;
     
+    // Quick validation, i.e if left blank or 0 function does not perform
+    // NOTE: "===" strictly equals to value "==" comparing values
+    if (billAmount === "" || serviceQuality == 0 ) {
+        return alert("Please Enter Amount."); // This will prevent the function from continuing
+    };
+    
+    // Checking to see if input is empty or less than or equal to 1
+    if (numPeople === "" || numPeople <= 1) {
+        // Took the var numPeople and added value of 1; if input is 0, 1 or nothing it will perform the same function for those input values
+        numPeople = 1; // Stored value of 1 to var numPeople
+        
+        document.getElementById("each").style.display = "none";
+    } else {
+        document. getElementById("each").style.display = "block";
+        
+    }
+    
+    // MATH
+    var total = ( billAmount * serviceQuality ) / numPeople;
+    // This will round amount to 2 decimal places
+    total = Math.round(total * 100) / 100;
+    // This will allow total amount to always show 2 decimal places
+    total = total.toFixed(2);
+    alert(total);
 };
 
 // Hide tip amount on load
@@ -19,5 +43,5 @@ document.getElementById("each").style.display = "none";
 
 // Clicking button calls on function
 document.getElementById("calculate").onclick = function() {
-    calculateTip();
+    calculateTip(); // Calling function from top of script to perform when button is clicked
 };

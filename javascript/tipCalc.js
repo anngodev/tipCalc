fdc40aacebd3af77fe835ff9553da63c0a554304
Andy <ngoan_230@yahoo.com>
2020-02-19 15:24:38 -0500

    Javascript functions to get values

--- javascript/tipCalc.js
+++ javascript/tipCalc.js
@@ -1,1 +1,23 @@
 /* Tip Calculator JS */
+
+// Build functions at top of script in one place
+
+// Custom fuction
+function calculateTip() {
+    
+    // Store data of inputs
+    var billAmount = document.getElementById("billAmount").value;
+    // NOTE: id of HTML is same as variable name to keep it consitant and less confusing, *preference*
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople = document.getElementById("totalPeople").value;
+    
+};
+
+// Hide tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Clicking button calls on function
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+};
